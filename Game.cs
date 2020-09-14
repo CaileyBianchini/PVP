f6c5b5363435f0dfd4f40ea12b2f02242ea03169
Cailey Bianchini <cailey.m.bianchini@gmail.com>
2020-09-14 10:50:14 -0500

    battle loop fin~

--- Game.cs
+++ Game.cs
@@ -39,14 +39,19 @@ namespace HelloWorld
         //Run the game
         public void Run()
         {
-
             Start();
-
-            while (_gameOver == false) ;
+            while (_gameOver == false)
             {
                 Update();
             }
             End();
+        }
+
+        public void Continue()
+        {
+            Console.WriteLine("Press [Enter] to continue.");
+            Console.ReadKey();
+            Console.Clear();
         }
 
         public void InitializePlayers()
@@ -123,9 +128,60 @@ namespace HelloWorld
                 _player2.damage += dagger.statBoost;
             }
 
-            Console.WriteLine("\nPlayer One.");
-            PrintStats(_player1);
+            Console.WriteLine("\nPlayer two.");
+            PrintStats(_player2);
+
+            Continue();
         }
+
+
+        public void StartBattle()
+        {
+            Console.WriteLine("\nNow......GO! FIGHT TO THE DEATH!");
+
+            while(_player1.health > 0 &&  _player2.health > 0)
+            {
+                //prints player one and two's stat
+                PrintStats(_player1);
+                PrintStats(_player2);
+
+                char input;
+
+                //Player One
+                GetInput(out input, "Attack", "Peace", "Player one! What do you wish to do?");
+
+                if(input == '1')
+                {
+                    _player2.health -= _player1.damage;
+                    Console.WriteLine("\nPlayer one dealt " + _player1.damage + " damage to Player two!");
+                }
+                if(input == '2')
+                {
+                    Console.WriteLine("Player one went with a peaceful option! Hopefully Player two feels the same!");
+
+                }
+
+                Continue();
+
+                //Player Two
+                GetInput(out input, "Attack", "Peace", "Player two! What do you wish to do?");
+
+                if (input == '1')
+                {
+                    _player1.health -= _player2.damage;
+                    Console.WriteLine("\nPlayer two dealt " + _player2.damage + " damage to Player one!");
+                }
+                if (input == '2')
+                {
+                    Console.WriteLine("Player two went with a peaceful option! Hopefully Player one feels the same!");
+
+                }
+
+                Continue();
+            }
+        }
+
+
 
         //Performed once when the game begins
         public void Start()
@@ -138,6 +194,7 @@ namespace HelloWorld
         public void Update()
         {
             EquipItems();
+            StartBattle();
         }
 
         //Performed once when the game ends

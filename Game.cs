b2f3bc6088637358a4f210d487aa0c6df0c0a728
Cailey Bianchini <cailey.m.bianchini@gmail.com>
2020-09-16 10:54:12 -0500

    I DID THE ROLES!!!!

--- Game.cs
+++ Game.cs
@@ -14,6 +14,7 @@ namespace HelloWorld
     struct Items
     {
         public int statBoost;
+        public string statName;
     }
 
 
@@ -21,10 +22,15 @@ namespace HelloWorld
     {
         bool _gameOver = false;
 
-        private Player _player1 = new Player("Karen", 177, 013);
-        private Player _player2 = new Player("Bob", 420, 69);
+        private Player _player1;
+        private Player _player2;
         private Items _longSword;
         private Items _dagger;
+        private Items _ax;
+        private Items _hero;
+        private Items _assassin;
+        private Items _wizard;
+        
 
 
         //Run the game
@@ -40,7 +46,7 @@ namespace HelloWorld
 
         public void Continue()
         {
-            Console.WriteLine("Press [Enter] to continue.");
+            Console.WriteLine("\nPress [Enter] to continue.");
             Console.ReadKey();
             Console.Clear();
         }
@@ -49,9 +55,18 @@ namespace HelloWorld
         {
             _longSword.statBoost = 15;
             _dagger.statBoost = 10;
+            _ax.statBoost = 13;
         }
 
-
+        public void InitializeRole()
+        {
+            _hero.statBoost = 20;
+            _hero.statName = "Hero";
+            _assassin.statBoost = 13;
+            _assassin.statName = "Assassin";
+            _wizard.statBoost = 5;
+            _wizard.statName = "Wizard";
+        }
 
 
         // INPUTS
@@ -74,14 +89,36 @@ namespace HelloWorld
                 }
             }
 
+            Continue();
         }
 
+        public void GetInput(out char input, string option1, string option2, string option3, string quiry)
+        {
+            Console.WriteLine(quiry);
+            Console.WriteLine("1. " + option1);
+            Console.WriteLine("2. " + option2);
+            Console.WriteLine("3. " + option3);
+            input = ' ';
+
+            //loops till valid is received
+            while (input != '1' && input != '2' && input != '3')
+            {
+                input = Console.ReadKey().KeyChar;
+                if (input != '1' && input != '2' && input != '3')
+                {
+                    Console.WriteLine("Invalid Input. Please Try Again.");
+
+                }
+            }
+
+            Continue();
+        }
+
+        //Weapons
         public void SelectItems(Player player)
         {
             char input;
-
-            //player 1
-            GetInput(out input, "Long Sword", "Dagger", "Welcome! Player one CHOOSE YOUR WEAPON!");
+            GetInput(out input, "Long Sword", "Dagger", "Ax", "Welcome! CHOOSE YOUR WEAPON!");
 
             if (input == '1')
             {
@@ -91,17 +128,47 @@ namespace HelloWorld
             {
                 player.EquipItem(_dagger);
             }
+            if (input == '3')
+            {
+                player.EquipItem(_ax);
+            }
  
         }
 
+        //Role
+        public void SelectRoles(Player player)
+        {
+            char input;
+            GetInput(out input, "Hero", "Assassin", "Wizard", "CHOOSE YOUR CLASS!");
+
+            if (input == '1')
+            {
+                player.EquipRole(_hero);
+            }
+            if (input == '2')
+            {
+                player.EquipRole(_assassin);
+            }
+            if (input == '3')
+            {
+                player.EquipRole(_wizard);
+            }
+
+        }
+
+        //Name
         public Player CreateCharacter()
         {
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
-            Player player = new Player(name, 100, 10);
+            Player player = new Player(name, "Peasant", 100, 10);
             SelectItems(player);
+            SelectRoles(player);
             return player;
         }
+
+        
+
 
         public void StartBattle()
         {
@@ -144,6 +211,7 @@ namespace HelloWorld
                     Console.WriteLine("\nPlayer two went with a peaceful option! Hopefully Player one feels the same!");
 
                 }
+                
 
                 Continue();
             }
@@ -166,6 +234,7 @@ namespace HelloWorld
         {
             //InitializePlayers();
             InitializeItems();
+            InitializeRole();
         }
 
         //Repeated until the game ends
@@ -179,7 +248,7 @@ namespace HelloWorld
         //Performed once when the game ends
         public void End()
         {
-
+            Console.WriteLine("Battle Over!");
         }
     }
 }
--- Player.cs
+++ Player.cs
@@ -7,29 +7,44 @@ namespace HelloWorld
     class Player
     {
         private string _name;
+        private string _roleName;
         private int _health;
         private int _damage;
+        //private string _roleName;
+
         public Player()
         {
+            _roleName = "Peasant";
             _health = 100;
             _damage = 10;
         }
-        public Player(string nameVal, int healthVal, int damageVal)
+        public Player(string nameVal, string roleVal, int healthVal, int damageVal)
         {
             _name = nameVal;
+            _roleName = roleVal;
             _health = healthVal;
             _damage = damageVal;
         }
+
+        
         
         public void EquipItem(Items weapon)
         {
             _damage += weapon.statBoost;
         }
 
+        public void EquipRole(Items role)
+        {
+            _damage += role.statBoost;
+            _roleName = role.statName;
+        }
+
         public string GetName()
         {
             return _name;
         }
+
+       //need to find out how to put role name
 
         public bool GetIsAlive()
         {
@@ -44,6 +59,7 @@ namespace HelloWorld
         public void PrintStats()
         {
             Console.WriteLine("Name: " + _name);
+            Console.WriteLine("Role: " + _roleName);
             Console.WriteLine("Health: " + _health);
             Console.WriteLine("Damage: " + _damage);
         }
